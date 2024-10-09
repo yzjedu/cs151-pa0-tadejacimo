2bbf534e9eebe5bc4e4fe8ef739df40564a7e23f
theresadejacimo <tadejacimo@loyola.edu>
2024-10-09 15:05:15 -0400

    Comepleted PA0

---
+++ Main1.py
@@ -0,0 +1,8 @@
+grocery_spendings_month = float ('Ask how much customer spends per month on grceries')
+income_per_month = float ('Ask customer how much they make per month')
+
+percentage_of_income_spent_on_groceries = (grocery_spendings_month / income_per_month) * 100
+
+print(percentage_of_income_spent_on_groceries)
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+
+#Theresa DeJacimo
+#Course:  CS151, Professor Dr. Zelalem Jembre Yalew
+#Due Date: 9/18/2024
+#Programming Assignment 0
+#Problem Statement: What percentage of my mom's monthly income goes to groceries for the month?
+#Data In: monthly income, money spent on groceries per month
+#Data Out: percentage % of money spent on groceries per month
+#Credits: High school Algebra class
+
+
+grocery_spendings_for_month = float(input('Ask customer for monthly grocery spendings'))
+income_for_month = float(input('Ask how much customer makes per month'))
+
+
+percentage_of_monthly_income_spent_on_groceries = (grocery_spendings_for_month / income_for_month) * 100
+
+
+print('Percentage of monthly income spent on groceries') percentage_of_monthly_income_spent_on_groceries
